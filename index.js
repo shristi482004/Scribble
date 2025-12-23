1f98199ea6f3218755c3d23f56df8bf2782d4e9a
shristi482004 <shristish484@gmail.com>
2025-12-23 13:05:27 +0530

    New Project from scratch

---
+++ index.js
@@ -0,0 +1,69 @@
+const express=require('express');
+const app=express();
+const Path=require('path');
+const file=require('fs');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static(Path.join(__dirname,'public')));
+app.set('view engine','ejs');
+app.use((req,res,next)=>{
+    console.log("MiddleWare Running");
+    next();
+});
+app.get('/',(req,res)=>{
+    file.readdir('files',(err,files)=>{
+         res.render('index',{files:files});
+    })
+   
+});
+
+app.post('/create',(req,res)=>{
+    file.writeFile(`files/${req.body.taskTitle.split(' ').join('')}.txt`,req.body.taskDescription,(err)=>{
+        if(err){
+            return res.status(500).send("Error creating file");
+        }
+       else console.log("File created successfully");
+        res.redirect('/');
+});
+});
+app.get('/files/:filename',(req,res)=>{
+    file.readFile(`./files/${req.params.filename}`,'utf-8',(err,files)=>{
+        if(err){
+            return res.status(500).send("Error reading file");
+        }
+      res.render('show',{filename:req.params.filename,description:files});
+    })
+   
+});
+app.get('/delete/:filename',(req,res)=>{
+    file.unlink(`./files/${req.params.filename}`, (err) => {
+    if (err) {
+        console.error('Error deleting file:', err);
+        return;
+    }
+    console.log('File deleted successfully!');
+    res.redirect('/');
+});
+    
+   
+});
+app.post('/edit/:prevname',(req,res)=>{
+   
+   file.rename(`./files/${req.body.prevname}`,`./files/${req.body.newName}`,(err)=>{
+        if(err){
+            return res.status(500).send("Error reading file");
+        }
+       
+      res.redirect('/');
+      
+    });
+   
+});
+app.use((err,req,res,next)=>{
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+})
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+});
